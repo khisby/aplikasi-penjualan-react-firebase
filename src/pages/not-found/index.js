039cee8273dd6f449881299a620dd040b8742ffb
Khisby <khisby@gmail.com>
2019-12-26 03:20:19 +0700

    add page not found

--- src/pages/not-found/index.js
+++ src/pages/not-found/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from 'react'
+import {Container,Paper,Typography} from '@material-ui/core'
+import {Link} from 'react-router-dom'
+import useStyle from './styles'
 
 function NotFound(){
-    return <h1> Halaman NotFound </h1>
+    const classes = useStyle()
+    return(
+        <Container maxWidth='xs'>
+            <Paper className={classes.paper}>
+                <Typography variant="subtitle1">
+                    Halaman Tidak Ditemukan
+                </Typography>
+                <Typography variant="h3">
+                    404
+                </Typography>
+                <Typography component={Link} to="/">
+                    Kembali ke Beranda
+                </Typography>
+            </Paper>
+        </Container>
+    )
 }
 
-export default NotFound;
+export default NotFound
---
+++ src/pages/not-found/styles.js
@@ -0,0 +1,11 @@
+import {makeStyles} from '@material-ui/styles'
+
+const useStyle = makeStyles(theme => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        padding: theme.spacing(6),
+        textAlign: 'center'
+    }
+}))
+
+export default useStyle
